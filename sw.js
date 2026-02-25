32b810ff2f8437a074ef3e75309ed5f87e1d7cdc
ldcom1993-hue <ldcom1993@gmail.com>
2026-02-26 00:10:32 +0100

    Create first playable mobile pumptrack PWA game

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_VERSION = "v1";
+const CACHE_NAME = `pump-flow-${CACHE_VERSION}`;
+const CORE_ASSETS = ["./", "./index.html", "./styles.css", "./game.js", "./manifest.webmanifest", "./assets/icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) => Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const networkFetch = fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || networkFetch;
+    })
+  );
+});
